acf0b3f75d244d182302f3c92dbca6aa9b41a65e
Mani <manisharma7309@gmail.com>
2025-08-11 21:19:12 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,20 @@
+public class DataType {
+    public static void main(String[] args){
+        int a = 5;
+        float f = 34.4f;
+        double d = 344.33;
+        char c = 'D';
+        String s = "Mani";
+        boolean b = true;
+        short w = 3344;
+        long l = 232323239;
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(w);
+        System.out.println(l);
+    }
+}
