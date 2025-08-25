f22c024447d9e65adedcd1ef2116241c38f33ec4
Mani Sharma <manisharma7309@gmail.com>
2025-08-25 11:08:11 +0530

    Add files via upload

---
+++ Conditiona.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Conditiona {
+    public void check() {
+        Scanner sc = new Scanner(System.in); // Scanner object
+        System.out.print("Enter your marks: ");
+        int marks = sc.nextInt(); // user input
+
+        if (marks == 10) {
+            System.out.println("O");
+        } else if (marks == 9) {
+            System.out.println("A+");
+        } else if (marks == 8) {
+            System.out.println("A");
+        } else {
+            System.out.println("Other grade");
+        }
+
+        sc.close(); 
+    }
+
+    public static void main(String[] args) {
+        Conditiona c = new Conditiona();
+        c.check();
+    }
+}
